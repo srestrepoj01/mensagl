32947e89892e1989e2ebf753205ded06a5a5fa1c
SebastianRJA <93653725+SebastianRJA@users.noreply.github.com>
2025-02-03 10:10:35 +0100

    PRUEBAS

--- AWS-CLI/vpc_y_sg+instancias+servicios.sh
+++ AWS-CLI/vpc_y_sg+instancias+servicios.sh
@@ -4,9 +4,14 @@
 #            VPC             #
 ##############################
 
-# Variables
+# Variables VPC
 NOMBRE_ALUMNO="equipo5" # Cambiar por la que sea del alumno
 REGION="us-east-1"
+
+# Variables DDNS
+#TOKEN= ""
+#SUB_DOMINIO_1= ""
+#SUB_DOMINIO_2= ""
 
 # Crear clave SSH
 aws ec2 create-key-pair \
@@ -15,9 +20,23 @@ aws ec2 create-key-pair \
     --output text > "ssh-mensagl-2025-${NOMBRE_ALUMNO}.pem"
 chmod 400 "ssh-mensagl-2025-${NOMBRE_ALUMNO}.pem"
 
+# Variables AMI-ID (Ubuntu server 24.04) y CLAVE SSH
+KEY_NAME="ssh-mensagl-2025-${NOMBRE_ALUMNO}"
+AMI_ID="ami-04b4f1a9cf54c11d0" # Llamar variable claves         
+
+# Variables for RDS, se pueden cambiar los valores por los deseados
+RDS_INSTANCE_ID="wordpress-db"
+DB_NAME="wp_db" 
+DB_USERNAME="admin"
+DB_PASSWORD="Admin123"
+
+
+##############################                       
+#             VPC             #
+##############################
 # Crear VPC
 VPC_ID=$(aws ec2 create-vpc --cidr-block "10.0.0.0/16" --query 'Vpc.VpcId' --output text)
-aws ec2 create-tags --resources "$VPC_ID" --tags Key=Name,Value="vpc-mensagl-2025-${NOMBRE_ALUMNO}-vpc"
+aws ec2 create-tags --resources "$VPC_ID" --tags Key=Name,Value="vpc-mensagl-2025-${NOMBRE_ALUMNO}"
 
 # Crear Subnets publicas
 SUBNET_PUBLIC1_ID=$(aws ec2 create-subnet --vpc-id "$VPC_ID" --cidr-block "10.0.1.0/24" --availability-zone "${REGION}a" --query 'Subnet.SubnetId' --output text)
@@ -113,3 +132,74 @@ aws ec2 authorize-security-group-egress --group-id "$SG_JITSI_ID" --protocol -1 
 SG_RDS_MYSQL_ID=$(aws ec2 create-security-group --group-name "sg_rds_mysql" --description "SG para el RDS del CMS" --vpc-id "$VPC_ID" --query 'GroupId' --output text)
 aws ec2 authorize-security-group-ingress --group-id "$SG_RDS_MYSQL_ID" --protocol tcp --port 3306 --cidr "0.0.0.0/0"
 aws ec2 authorize-security-group-egress --group-id "$SG_RDS_MYSQL_ID" --protocol -1 --port all --cidr "0.0.0.0/0"
+
+
+##############################                       
+# Crear instancias EC2 + RDS #
+##############################
+
+# proxy-zona1
+# ====== Variables ======
+INSTANCE_NAME="proxy-zona1"             # Tag: NOMBRE DE LA INSTANCIA
+SUBNET_ID="${SUBNET_PUBLIC1}"           # ID DE LA SUBNET
+SECURITY_GROUP_ID="${SG_ID_PROXY}"      # ID SG
+PRIVATE_IP="10.0.1.10"                  # IP PRIVADA INSTANCIA
+
+INSTANCE_TYPE="t2.micro"                # TIPO DE LA INSTANCIA
+KEY_NAME="${KEY_NAME}"                  # CLAVES SSH
+VOLUME_SIZE=8                           # TAMAÑO VOLUMEN
+USER_DATA_SCRIPT=$(cat <<'EOF'
+# UNA VEZ SE TENGA, AÑADIR SCRIPT
+EOF
+)
+# ====== Create EC2 Instance ======
+INSTANCE_ID=$(aws ec2 run-instances \
+    --image-id "$AMI_ID" \
+    --instance-type "$INSTANCE_TYPE" \
+    --key-name "$KEY_NAME" \
+    --block-device-mappings "DeviceName=/dev/sda1,Ebs={VolumeSize=$VOLUME_SIZE,VolumeType=gp3,DeleteOnTermination=true}" \
+    --network-interfaces "SubnetId=$SUBNET_ID,AssociatePublicIpAddress=true,DeviceIndex=0,PrivateIpAddresses=[{Primary=true,PrivateIpAddress=$PRIVATE_IP}],Groups=[$SECURITY_GROUP_ID]" \
+    --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$INSTANCE_NAME}]" \
+    --user-data "$(echo "$USER_DATA_SCRIPT" | sed "s/\$TOKEN/$TOKEN/g" | sed "s/\$DUCKDNS_SUBDOMAIN/$SUB_DOMINIO_1/g")" \
+    --query "Instances[0].InstanceId" \
+    --output text)
+echo "${INSTANCE_NAME} created";
+
+# PROXY-2
+# ====== Variables ======
+INSTANCE_NAME="proxy-zona2"         # Tag: NOMBRE DE LA INSTANCIA             
+SUBNET_ID="${SUBNET_PUBLIC2}"       # ID DE LA SUBNET     
+SECURITY_GROUP_ID="${SG_ID_PROXY}"  # ID SG
+PRIVATE_IP="10.0.2.10"              # IP PRIVADA INSTANCIA  
+
+INSTANCE_TYPE="t2.micro"            # TIPO DE LA INSTANCIA    
+KEY_NAME="${KEY_NAME}"              # CLAVES SSH   
+VOLUME_SIZE=8                       # TAMAÑO VOLUMEN   
+USER_DATA_SCRIPT=$(cat <<'EOF'
+# UNA VEZ SE TENGA, AÑADIR SCRIPT
+EOF
+)
+
+
+# ====== CREAR INSTANCIA ======
+INSTANCE_ID=$(aws ec2 run-instances \
+    --image-id "$AMI_ID" \
+    --instance-type "$INSTANCE_TYPE" \
+    --key-name "$KEY_NAME" \
+    --block-device-mappings "DeviceName=/dev/sda1,Ebs={VolumeSize=$VOLUME_SIZE,VolumeType=gp3,DeleteOnTermination=true}" \
+    --network-interfaces "SubnetId=$SUBNET_ID,AssociatePublicIpAddress=true,DeviceIndex=0,PrivateIpAddresses=[{Primary=true,PrivateIpAddress=$PRIVATE_IP}],Groups=[$SECURITY_GROUP_ID]" \
+    --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$INSTANCE_NAME}]" \
+    --user-data "$(echo "$USER_DATA_SCRIPT" | sed "s/\$TOKEN/$TOKEN/g" | sed "s/\$DUCKDNS_SUBDOMAIN2/$SUB_DOMINIO_2/g")" \
+    --query "Instances[0].InstanceId" \
+    --output text)
+echo "${INSTANCE_NAME} created";
+
+
+
+
+
+
+
+
+
+
