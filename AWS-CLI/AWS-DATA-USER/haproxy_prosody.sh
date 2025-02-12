b21136ac37fb509919b5fbe7245e4327887e9c9b
SebastianRJA <93653725+SebastianRJA@users.noreply.github.com>
2025-02-12 12:50:20 +0100

    Update haproxy_prosody.sh

--- AWS-CLI/AWS-DATA-USER/haproxy_prosody.sh
+++ AWS-CLI/AWS-DATA-USER/haproxy_prosody.sh
@@ -3,70 +3,73 @@
 # Variables
 HAPROXY_CFG_PATH="/etc/haproxy/haproxy.cfg"
 BACKUP_CFG_PATH="/etc/haproxy/haproxy.cfg.bak"
-
-# CONFIGURACION DUCKDNS
-
-# SEBASTIAN
-DUCKDNS_DOMAIN="srestrepoj-prosody.duckdns.org" # CAMBIAR POR DOMINIO DE PROSODY
-DUCKDNS_TOKEN="d9c2144c-529b-4781-80b7-20ff1a7595de" # PONER TOKEN DE CUENTA
-
-SSL_PATH="/etc/letsencrypt/live/$DUCKDNS_DOMAIN"
-CERT_PATH="$SSL_PATH/fullchain.pem"
+DUCKDNS_DOMAIN="srestrepoj-wp"
+DUCKDNS_TOKEN="d9c2144c-529b-4781-80b7-20ff1a7595de"
+SSL_PATH="/etc/letsencrypt/live/${DUCKDNS_DOMAIN}"
+CERT_PATH="${SSL_PATH}/fullchain.pem"
+DUCKDNS_DIR="/home/ubuntu/duckdns"
+DUCKDNS_SCRIPT="${DUCKDNS_DIR}/duck.sh"
+DUCKDNS_LOG="${DUCKDNS_DIR}/duck.log"
 LOG_FILE="/var/log/script.log"
+EMAIL="srestrepoj01@educantabria.es"
 
-# Redirigir toda la salida a LOG_FILE
-exec > >(tee -a $LOG_FILE) 2>&1
+# Redirige la salida al archivo de registro
+exec > >(sudo tee -a "${LOG_FILE}") 2>&1
 
-# CONFIGURAR DUCKDNS
-mkdir -p /home/ubuntu/duckdns
+# Configuración de DuckDNS
+sudo mkdir -p "${DUCKDNS_DIR}"
 
-cat <<EOL > /home/ubuntu/duckdns/duck.sh
-echo url="https://www.duckdns.org/update?domains=$DUCKDNS_DOMAIN&token=$DUCKDNS_TOKEN&ip=" | curl -k -o /home/ubuntu/duckdns/duck.log -K -
+# Crea el script de actualización de DuckDNS
+sudo tee "${DUCKDNS_SCRIPT}" > /dev/null <<EOL
+#!/bin/bash
+echo url="https://www.duckdns.org/update?domains=${DUCKDNS_DOMAIN}&token=${DUCKDNS_TOKEN}&ip=" | curl -k -o "${DUCKDNS_LOG}" -K -
 EOL
 
-sudo chown ubuntu:ubuntu /home/ubuntu/duckdns/duck.sh
-sudo chmod 700 /home/ubuntu/duckdns/duck.sh
+# Cambia la propiedad y los permisos del script
+sudo chown ubuntu:ubuntu "${DUCKDNS_SCRIPT}"
+sudo chmod 700 "${DUCKDNS_SCRIPT}"
 
-# Agregar el cron job para ejecutar el script cada 5 minutos
-(crontab -l 2>/dev/null; echo "*/5 * * * * /home/ubuntu/duckdns/duck.sh >/dev/null 2>&1") | crontab -
+# Agrega la tarea al crontab para ejecutarse al reiniciar
+CRON_JOB="@reboot ${DUCKDNS_SCRIPT} >/dev/null 2>&1"
+(crontab -l 2>/dev/null; echo "${CRON_JOB}") | crontab -
 
-# Probar el script
-sudo chmod +x /home/ubuntu/duckdns/duck.sh
-sudo /home/ubuntu/duckdns/duck.sh
+# Prueba el script de actualización
+sudo chmod +x "${DUCKDNS_SCRIPT}"
+sudo "${DUCKDNS_SCRIPT}"
 
-# Verificar el resultado del último intento
-cat /home/ubuntu/duckdns/duck.log
+# Verifica el resultado del intento
+sudo cat "${DUCKDNS_LOG}"
 
-# INSTALACION DE CERTBOT
-sudo apt update && sudo DEBIAN_FRONTEND=noninteractive apt install certbot -y
+# Instala Certbot
+sudo apt update && sudo DEBIAN_FRONTEND=noninteractive apt install -y certbot
 
-# CONFIGURACION DE LET'S ENCRYPT (Certbot)
-if [ -f "$CERT_PATH" ]; then
+# Configuración de Let's Encrypt
+if [ -f "${CERT_PATH}" ]; then
     sudo certbot renew --non-interactive --quiet
 else
-    sudo certbot certonly --standalone -d $DUCKDNS_DOMAIN --non-interactive --agree-tos -m admin@$DUCKDNS_DOMAIN
+    sudo certbot certonly --standalone -d "${DUCKDNS_DOMAIN}" --non-interactive --agree-tos --email "${EMAIL}"
 fi
 
-# FUSIONAR ARCHIVOS DE CERTIFICADO
-sudo cat /etc/letsencrypt/live/$DUCKDNS_DOMAIN/fullchain.pem /etc/letsencrypt/live/$DUCKDNS_DOMAIN/privkey.pem | sudo tee /etc/letsencrypt/live/$DUCKDNS_DOMAIN/haproxy.pem > /dev/null
+# Combina los archivos de certificado para HAProxy
+sudo cat "${SSL_PATH}/fullchain.pem" "${SSL_PATH}/privkey.pem" | sudo tee "${SSL_PATH}/haproxy.pem" > /dev/null
 
-# DAR PERMISOS AL CERTIFICADO
-sudo chmod 644 /etc/letsencrypt/live/$DUCKDNS_DOMAIN/haproxy.pem
-sudo chmod 755 -R /etc/letsencrypt/live/$DUCKDNS_DOMAIN
+# Establece permisos para el certificado
+sudo chmod 644 "${SSL_PATH}/haproxy.pem"
+sudo chmod 755 -R "${SSL_PATH}"
 sudo chmod 755 /etc/letsencrypt/live/
 
-# INSTALACION DE HAPROXY
+# Instala HAProxy
 sudo apt-get update
 sudo apt-get install -y haproxy
 
-# COPIA DE SEGURIDAD DE LA CONFIGURACION INICIAL
-sudo cp "$HAPROXY_CFG_PATH" "$BACKUP_CFG_PATH"
+# Crea una copia de seguridad de la configuración inicial de HAProxy
+sudo cp "${HAPROXY_CFG_PATH}" "${BACKUP_CFG_PATH}"
 
-# CONFIGURACION DE HAPROXY
-sudo tee "$HAPROXY_CFG_PATH" > /dev/null <<EOL
+# Configura HAProxy
+sudo tee "${HAPROXY_CFG_PATH}" > /dev/null <<EOL
 global
-    log /dev/log    local0
-    log /dev/log    local1 notice
+    log /dev/log local0
+    log /dev/log local1 notice
     chroot /var/lib/haproxy
     stats socket /run/haproxy/admin.sock mode 660 level admin expose-fd listeners
     stats timeout 30s
@@ -75,13 +78,13 @@ global
     daemon
 
 defaults
-    log     global
-    mode    http
-    option  httplog
-    option  dontlognull
+    log global
+    mode http
+    option httplog
+    option dontlognull
     timeout connect 5000ms
-    timeout client  50000ms
-    timeout server  50000ms
+    timeout client 50000ms
+    timeout server 50000ms
     errorfile 400 /etc/haproxy/errors/400.http
     errorfile 403 /etc/haproxy/errors/403.http
     errorfile 408 /etc/haproxy/errors/408.http
@@ -90,42 +93,23 @@ defaults
     errorfile 503 /etc/haproxy/errors/503.http
     errorfile 504 /etc/haproxy/errors/504.http
 
-frontend xmpp_front
-    bind *:5222 
-    bind *:5269
-    mode tcp
-    default_backend xmpp_back
-
-frontend http_xmpp
+frontend wordpress_front
     bind *:80
-    bind *:443 ssl crt /etc/letsencrypt/live/$DUCKDNS_DOMAIN/haproxy.pem
+    bind *:443 ssl crt ${SSL_PATH}/haproxy.pem
     mode http
     redirect scheme https if !{ ssl_fc }
-    default_backend http_back
+    default_backend wordpress_back
 
-backend xmpp_back
-    mode tcp
-    balance roundrobin
-    server mensajeria1 10.225.3.20:5222 check
-    server mensajeria2 10.225.3.20:5269 check
-    server mensajeria3 10.225.3.20:5270 check
-
-backend http_back
+backend wordpress_back
     mode http
     balance source
-    server mensajeria4 10.225.3.20:80 check
-
-backend db_back
-    mode tcp
-    balance roundrobin
-    server db_primary 10.225.3.10:3306 check
-    server db_secondary 10.225.3.11:3306 check backup
+    server wordpress1 10.225.4.10:80 check
+    server wordpress2 10.225.4.11:80 check
 EOL
 
-# REINICIAR Y HABILITAR HAPROXY
+# Reinicia y habilita HAProxy
 sudo systemctl restart haproxy
 sudo systemctl enable haproxy
 
-# VERIFICAR ESTADO DE HAPROXY
+# Verifica el estado de HAProxy
 sudo systemctl status haproxy --no-pager
-
