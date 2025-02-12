f56abb400606f26d97275a3b9b596e6ae0df4917
SebastianRJA <93653725+SebastianRJA@users.noreply.github.com>
2025-02-12 13:22:55 +0100

    Update vpc_y_sg+rds.sh

--- AWS-CLI/vpc_y_sg+rds.sh
+++ AWS-CLI/vpc_y_sg+rds.sh
@@ -333,24 +333,9 @@ PRIVATE_IP="10.225.3.20"
 USER_DATA_SCRIPT=$(cat <<EOF
 #!/bin/bash
 # Instalación de Prosody y configuración de base de datos MySQL externa.
+sleep 180
 
 LOG_FILE="/var/log/setup_script.log"
-
-# Función para verificar que una instancia esté activa y en funcionamiento
-check_instance_status() {
-    instance_ip=$1
-    status=$(aws ec2 describe-instance-status --instance-ids "$instance_ip" --query "InstanceStatuses[0].InstanceState.Name" --output text)
-    while [ "$status" != "running" ]; do
-        echo "Esperando a que la instancia con IP $instance_ip esté activa..." | tee -a $LOG_FILE
-        sleep 10
-        status=$(aws ec2 describe-instance-status --instance-ids "$instance_ip" --query "InstanceStatuses[0].InstanceState.Name" --output text)
-    done
-    echo "La instancia con IP $instance_ip está en funcionamiento." | tee -a $LOG_FILE
-}
-
-# Verificar el estado de las instancias de la base de datos
-check_instance_status "10.225.3.10"
-check_instance_status "10.225.3.11"
 
 # Instalación de Prosody
 echo "Instalando Prosody y módulos adicionales..." | tee -a $LOG_FILE
@@ -427,113 +412,92 @@ EOF
      --output text)
  echo "${INSTANCE_NAME} creada: ${INSTANCE_ID}"
 
-# mensajeria-2
-INSTANCE_NAME="mensajeria-2"
-SUBNET_ID="${SUBNET_PRIVATE1_ID}"
-SECURITY_GROUP_ID="${SG_MENSAJERIA_ID}"
-PRIVATE_IP="10.225.3.30"
-USER_DATA_SCRIPT=$(cat <<EOF
-#!/bin/bash
-# Instalación de Prosody y configuración de base de datos MySQL externa.
+# # mensajeria-2
+# INSTANCE_NAME="mensajeria-2"
+# SUBNET_ID="${SUBNET_PRIVATE1_ID}"
+# SECURITY_GROUP_ID="${SG_MENSAJERIA_ID}"
+# PRIVATE_IP="10.225.3.30"
+# USER_DATA_SCRIPT=$(cat <<EOF
+# #!/bin/bash
+# # Instalación de Prosody y configuración de base de datos MySQL externa.
+# LOG_FILE="/var/log/setup_script.log"
 
-# Variables
-db_host="10.225.3.10"
-db_user="admin"
-db_password="Admin123"
-db_name="prosody"
 
-LOG_FILE="/var/log/setup_script.log"
 
-# Función para verificar que una instancia esté activa y en funcionamiento
-check_instance_status() {
-    instance_ip=$1
-    status=$(aws ec2 describe-instance-status --instance-ids "$instance_ip" --query "InstanceStatuses[0].InstanceState.Name" --output text)
-    while [ "$status" != "running" ]; do
-        echo "Esperando a que la instancia con IP $instance_ip esté activa..." | tee -a $LOG_FILE
-        sleep 10
-        status=$(aws ec2 describe-instance-status --instance-ids "$instance_ip" --query "InstanceStatuses[0].InstanceState.Name" --output text)
-    done
-    echo "La instancia con IP $instance_ip está en funcionamiento." | tee -a $LOG_FILE
-}
+# # Instalación de Prosody
+# echo "Instalando Prosody y módulos adicionales..." | tee -a $LOG_FILE
+# sudo apt update
+# sudo apt install lua-dbi-mysql lua-dbi-postgresql lua-dbi-sqlite3 -y 
 
-# Verificar el estado de las instancias de la base de datos
-check_instance_status "10.225.3.10"
-check_instance_status "10.225.3.11"
+# # Configurar Prosody
+# echo "Configurando Prosody..." | tee -a $LOG_FILE
+# sudo tee /etc/prosody/prosody.cfg.lua > /dev/null <<EOL
+# -- Prosody Configuration
 
-# Instalación de Prosody
-echo "Instalando Prosody y módulos adicionales..." | tee -a $LOG_FILE
-sudo apt update
-sudo apt install lua-dbi-mysql lua-dbi-postgresql lua-dbi-sqlite3 -y 
+# VirtualHost "srestrepoj-prosody.duckdns.org"
+# admins = { "admin@srestrepoj-prosody.duckdns.org" }
 
-# Configurar Prosody
-echo "Configurando Prosody..." | tee -a $LOG_FILE
-sudo tee /etc/prosody/prosody.cfg.lua > /dev/null <<EOL
--- Prosody Configuration
+# modules_enabled = {
+#     "roster";
+#     "saslauth";
+#     "tls";
+#     "dialback";
+#     "disco";
+#     "posix";
+#     "private";
+#     "vcard";
+#     "version";
+#     "uptime";
+#     "time";
+#     "ping";
+#     "register";
+#     "admin_adhoc";
+# }
 
-VirtualHost "srestrepoj-prosody.duckdns.org"
-admins = { "admin@srestrepoj-prosody.duckdns.org" }
+# allow_registration = true
+# daemonize = true
+# pidfile = "/var/run/prosody/prosody.pid"
+# c2s_require_encryption = true
+# s2s_require_encryption = true
 
-modules_enabled = {
-    "roster";
-    "saslauth";
-    "tls";
-    "dialback";
-    "disco";
-    "posix";
-    "private";
-    "vcard";
-    "version";
-    "uptime";
-    "time";
-    "ping";
-    "register";
-    "admin_adhoc";
-}
+# log = {
+#     info = "/var/log/prosody/prosody.log";
+#     error = "/var/log/prosody/prosody.err";
+#     "*syslog";
+# }
 
-allow_registration = true
-daemonize = true
-pidfile = "/var/run/prosody/prosody.pid"
-c2s_require_encryption = true
-s2s_require_encryption = true
+# storage = "sql"
+# sql = {
+#     driver = "MySQL";
+#     database = "10.225.3.10";
+#     username = "admin";
+#     password = "Admin123";
+#     host = "prosody";
+# }
+# EOL
 
-log = {
-    info = "/var/log/prosody/prosody.log";
-    error = "/var/log/prosody/prosody.err";
-    "*syslog";
-}
+# # Reiniciar Prosody
+# echo "Reiniciando Prosody..." | tee -a $LOG_FILE
+# sudo systemctl restart prosody
 
-storage = "sql"
-sql = {
-    driver = "MySQL";
-    database = "10.225.3.10";
-    username = "admin";
-    password = "Admin123";
-    host = "prosody";
-}
-EOL
+# # Crear usuario administrador
+# echo "Creando usuario admin@srestrepoj-prosody.duckdns.org..." | tee -a $LOG_FILE
+# sudo prosodyctl register admin srestrepoj-prosody.duckdns.org "Admin123"
 
-# Reiniciar Prosody
-echo "Reiniciando Prosody..." | tee -a $LOG_FILE
-sudo systemctl restart prosody
-
-# Crear usuario administrador
-echo "Creando usuario admin@srestrepoj-prosody.duckdns.org..." | tee -a $LOG_FILE
-sudo prosodyctl register admin srestrepoj-prosody.duckdns.org "Admin123"
-
-echo "Prosody instalado y configurado con éxito en srestrepoj-prosody.duckdns.org" | tee -a $LOG_FILE
-EOF
-)
- INSTANCE_ID=$(aws ec2 run-instances \
-     --image-id "$AMI_ID" \
-     --instance-type "t2.medium" \
-     --key-name "$KEY_NAME" \
-     --block-device-mappings "DeviceName=/dev/sda1,Ebs={VolumeSize=$VOLUME_SIZE,VolumeType=gp3,DeleteOnTermination=true}" \
-     --network-interfaces "SubnetId=$SUBNET_ID,DeviceIndex=0,PrivateIpAddresses=[{Primary=true,PrivateIpAddress=$PRIVATE_IP}],Groups=[$SECURITY_GROUP_ID]" \
-     --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$INSTANCE_NAME}]" \
-     --user-data "$USER_DATA_SCRIPT" \
-     --query "Instances[0].InstanceId" \
-     --output text)
- echo "${INSTANCE_NAME} creada: ${INSTANCE_ID}"
+# echo "Prosody instalado y configurado con éxito en srestrepoj-prosody.duckdns.org" | tee -a $LOG_FILE
+# EOF
+# )
+#  INSTANCE_ID=$(aws ec2 run-instances \
+#      --image-id "$AMI_ID" \
+#      --instance-type "t2.medium" \
+#      --key-name "$KEY_NAME" \
+#      --block-device-mappings "DeviceName=/dev/sda1,Ebs={VolumeSize=$VOLUME_SIZE,VolumeType=gp3,DeleteOnTermination=true}" \
+#      --network-interfaces "SubnetId=$SUBNET_ID,DeviceIndex=0,PrivateIpAddresses=[{Primary=true,PrivateIpAddress=$PRIVATE_IP}],Groups=[$SECURITY_GROUP_ID]" \
+#      --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$INSTANCE_NAME}]" \
+#      --user-data "$USER_DATA_SCRIPT" \
+#      --query "Instances[0].InstanceId" \
+#      --output text)
+#  echo "${INSTANCE_NAME} creada: ${INSTANCE_ID}"
 
 ##############
 # WORDPRESS  #
@@ -548,6 +512,7 @@ USER_DATA_SCRIPT=$(cat <<EOF
 ##############################
 #  INSTALACION WP / PLUGINS  #
 ##############################
+sleep 180
 
 # Variables
 WP_PATH="/var/www/html"
